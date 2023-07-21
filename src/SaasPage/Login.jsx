7846a892c58f24a574eae00feba68a2606e61cd8
cenkozk <emirozkan61@hotmail.com>
2023-07-22 02:41:41 +0300

    Update

--- src/SaasPage/Login.jsx
+++ src/SaasPage/Login.jsx
@@ -18,7 +18,6 @@ function Login() {
     try {
       const { user, session, error } = await supabase.auth.signInWithOAuth({
         provider: "google",
-        redirectTo: `https://digidish.vercel.app/dashboard/`, // Redirect to the dashboard after successful login
       });
 
       if (error) {
